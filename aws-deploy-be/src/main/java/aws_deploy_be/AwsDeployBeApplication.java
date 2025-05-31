d7e1542e66927233a4eb4460d38a08d9066818e4
chygai232 <chygai232@gmail.com>
2025-05-31 21:01:20 +0900

    first commit

---
+++ aws-deploy-be/src/main/java/aws_deploy_be/AwsDeployBeApplication.java
@@ -0,0 +1,13 @@
+package aws_deploy_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeployBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDeployBeApplication.class, args);
+	}
+
+}
---
+++ aws-deploy-be/src/test/java/aws_deploy_be/AwsDeployBeApplicationTests.java
@@ -0,0 +1,13 @@
+package aws_deploy_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeployBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
