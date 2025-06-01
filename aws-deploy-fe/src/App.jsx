cbbad1d220464d93e769a377a0236f876e2c136b
chygai232 <chygai232@gmail.com>
2025-06-01 22:33:04 +0900

    update user list feature

--- aws-deploy-fe/src/App.jsx
+++ aws-deploy-fe/src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState({
+    name: '',
+    email: '',
+  });  
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    getUserList();
+    return () => {}
+  }, []);
+
+  async function getUserList() {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/user/all`);
+    if(response.status === 200) {
+      const jsonData = await response.json();
+      setUserList(jsonData);
+    }
+  }
+
+  const inputHandler = function(e) {
+    setInput((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      }
+    });
+  };
+
+  const submitHandler = async function(e) {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/user/add`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(input),
+      }
+    );
+
+    if(response.status === 200) {
+      const jsonData = await response.json();
+      console.log(jsonData);
+      alert('정상 처리되었습니다.');
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="form">
+        <input type="text" name="name" placeholder="name" onChange={inputHandler} />
+        <input type="text" name="email" placeholder="email" onChange={inputHandler} />
+        <input type="submit" name="submit" value="submit" onClick={submitHandler} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <table>
+        <caption>
+          AWS Tutorial User List 
+        </caption>
+        <thead>
+          <tr>
+            <th scope="col">NAME</th>
+            <th scope="col">EMAIL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            userList && userList.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <th scope="row">{item.name}</th>
+                  <td>{item.email}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
